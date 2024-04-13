eac790a5aa639123efec196266c59eb68ee1d54d
hart-venus <aleyva1509@gmail.com>
2024-04-13 05:00:25 -0600

    add: deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  site: 'https://hart-venus.github.io',
+  base: 'contexto-3-factory',
 });
